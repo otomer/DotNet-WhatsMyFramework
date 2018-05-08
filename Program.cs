b188f7ac7b7c68ee848f1aa3a2684dea85ad4d77
Tomer Ovadia <tomero@liveperson.com>
2018-05-08 15:25:58 +0300

    Adding WhatsMyDotNetFramework project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TO.WhatsMyDotNetFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Environment Information");
+            Console.WriteLine("-----------------------");
+
+            GetDotNetVersion.Get45PlusFromRegistry();
+            Console.WriteLine($"Environment Version: {Environment.Version}");
+
+            Console.WriteLine($"{Environment.NewLine}You've been using tomero's tool to let you know which .NET version you have installed. :-)");
+            AsciiArt.Draw();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> Key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
